bfeec908bbc417110f719ce15fb176b53d479cc8
e-mandy <andymfrd02@gmail.com>
2026-01-21 17:08:16 +0100

    feat(reset-password): Mise en place dela méthode d'envoi de mail de reset de password

--- src/application/use-cases/RequestPasswordReset.ts
+++ src/application/use-cases/RequestPasswordReset.ts
@@ -1,20 +1,30 @@
 import { AppError } from "../../domain/exceptions/AppError.ts";
 import type { IUserRepositories } from "../../domain/repositories/UserRepositories.ts";
 import type { IMailerService } from "../../domain/services/IMailerService.ts";
+import jwt from 'jsonwebtoken';
 
 export class RequestPasswordReset {
     private userRepository: IUserRepositories;
-    mailService: IMailerService;
+    private mailService: IMailerService;
+    private secretResetPassword: string | null;
 
     constructor(repository: IUserRepositories, mailServices: IMailerService){
         this.userRepository = repository;
         this.mailService = mailServices;
+        this.secretResetPassword = process.env.SECRET_PASSWORD_RESET_APP || null;
     }
 
-    execute(email: string){
-        const result = this.userRepository.findByEmail(email);
+    async execute(email: string){
+        if(!this.secretResetPassword){
+            console.log("Reset password secret key not available");
+            throw new AppError('INTERNAL SERVER ERROR', 500);
+        }
+
+        const result = await this.userRepository.findByEmail(email);
         if(!result) throw new AppError("INVALID CREDENTIALS", 400);
 
+        const newRequestResetToken = jwt.sign({ userId: result.id }, this.secretResetPassword, { expiresIn: "30m"})
 
+        this.mailService.sendPasswordResetToken(email, newRequestResetToken);
     }
 }
