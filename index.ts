703f903584c519bc0bbcbb2d17d0ca27ff7a52a7
e-mandy <andymfrd02@gmail.com>
2026-01-23 18:51:32 +0100

    feat(cron): Mise en place du cron pour le nettoyage de la base de données

--- index.ts
+++ index.ts
@@ -1,6 +1,10 @@
 import express from 'express';
 import authRoutes from './src/infrastructure/web/routes/authRoutes.ts';
 import { errorMiddleware } from './src/infrastructure/web/middleware/errorMiddleware.ts';
+import cron from 'node-cron';
+import { PrismaUserRepository } from './src/infrastructure/repositories/PrismaUserRepository.ts';
+
+const userRepository = new PrismaUserRepository();
 
 const app = express();
 
@@ -9,6 +13,11 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 
 app.use(errorMiddleware);
+
+cron.schedule('0 0 * * *', () => {
+    console.log("Nettoyage de la base de données enclenché.");
+    userRepository.cleanupExpiredTokens();
+});
 
 app.listen(3000, () => {
     console.log("Server is running on the port 3000.")
